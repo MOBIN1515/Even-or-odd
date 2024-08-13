6d128506e4b801718581fa27a713205eb4506523
Mobin <Mobin@DESKTOP-NIS2M55>
2024-08-13 14:37:04 +0330

    Add project files.

---
+++ Even or odd/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.Design;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Even_or_odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter number1 :");
+            string a = Console.ReadLine();
+            int number = int.Parse(a);
+            
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine(" your number is even.");
+            }
+            else
+            {
+                Console.WriteLine("number is odd.");
+            }
+        }
+    }
+} 
